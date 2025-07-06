d3a6ca9354f30da52f22f063525d240d719c0e18
Aman Kumar <105776674+Amankumaraman@users.noreply.github.com>
2025-07-06 13:12:38 +0530

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,12 @@
-import '../public/output.css'; // Import the compiled Tailwind CSS
+import '../public/output.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} closeOnClick draggable pauseOnHover />
+    </>
+  );
 }
